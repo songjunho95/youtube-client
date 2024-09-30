aa1dd97020999a020512ae153780b363a0805958
songjunho95 <songpro3290@naver.com>
2024-09-30 09:50:53 +0900

    adad

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,2 +1,26 @@
-const Main = () => {};
+import "../assets/style.css";
+import { getVideos } from "../api/video";
+import {useState, useEffect} from "react";
+
+
+const Main = () => {
+      const [videos, setVideos] = useState([]);
+      const videoAPI = async () => {
+        const result = await getVideos();
+        setVideos(result.data);
+      };
+      useEffect(()=> {
+        videoAPI();
+      }, []);
+    return(
+        <section>
+            {videos.map((video) => (
+                <div className="", key={video,video.Code}
+            ))}
+
+        </section>
+
+    );
+
+};
 export default Main;
